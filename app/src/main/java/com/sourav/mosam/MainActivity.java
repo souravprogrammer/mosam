056d81fa27dc71be0a1935696151fb4c40470bdb
sourav sharma <souravsh1234567@gmail.com>
2021-03-29 00:39:00 +0530

    Creating project

---
+++ app/src/main/java/com/sourav/mosam/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sourav.mosam;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Entity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
